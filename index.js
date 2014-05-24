1c70d705d0cb1c530998a849bc75a05a77b5daa6
Trey Eckels <trey.eckels@gmail.com>
2014-05-24 15:18:32 -0400

    travis

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('hi!');
+var JSFtp = require("jsftp");
+
+var Ftp = new JSFtp({
+    host: process.argv[4],
+    user: process.argv[2],
+    pass: process.argv[3]
+});
+
+Ftp.raw.mkd("/" + process.argv[4], function(err, data) {
+    if (err) return console.error(err);
+
+    console.log(data.text); // Show the FTP response text to the user
+    console.log(data.code); // Show the FTP response code to the user
+
+    Ftp.put('dist', "/" + process.argv[4], function(hadError) {
+        if (!hadError)
+            console.log("Files transferred successfully!");
+    });
+
+});
