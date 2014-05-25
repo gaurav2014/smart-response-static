c9877425fd3495d0bd41ea00df21b59d9fa72e09
Trey Eckels <trey.eckels@gmail.com>
2014-05-25 03:40:01 -0400

    travis

--- index.js
+++ index.js
@@ -1,82 +1,24 @@
-var JSFtp = require("jsftp"),
-    fs = require('fs');
+var FtpDeploy = require('ftp-deploy');
+var ftpDeploy = new FtpDeploy();
 
-var Ftp = new JSFtp({
+var config = {
+    username: process.argv[2],
+    password: process.argv[3], // optional, prompted if none given
     host: process.argv[4],
-    user: process.argv[2],
-    pass: process.argv[3]
-});
+    port: 21,
+    localRoot: __dirname + "/dist",
+    remoteRoot: process.argv[5] ,
+    exclude: ['.git', '.idea', 'tmp/*']
+}
 
-var traverseFileSystem = function (currentPath) {
-    console.log(currentPath);
-    var files = fs.readdirSync(currentPath);
-    for (var i in files) {
-        var currentFile = currentPath + '/' + files[i];
-        var stats = fs.statSync(currentFile);
-        if (stats.isFile()) {
-            console.log(currentFile);
-            uploadFile(currentFile);
-        }
-        else if (stats.isDirectory()) {
-            Ftp.raw.mkd("/" + process.argv[5] + '/' + currentFile, function(err, data) {
-                if (err) return console.error(err);
 
-                console.log(data.text); // Show the FTP response text to the user
-                console.log(data.code); // Show the FTP response code to the user
 
-                traverseFileSystem(currentFile);
-
-            });
-
-        }
-    }
-};
-
-Ftp.auth(process.argv[2], process.argv[3], function(err,data){
-    if (err) return console.error(err);
-    console.log('checking for folder');
-    Ftp.ls(process.argv[5],function(err,data){
-        if(err) console.log(err);
-        console.log(data);
-        if(!data.length){
-            console.log('folder does not exist');
-            makeDir();
-        } else {
-            Ftp.raw.rmd('process.argv[5]',function(err,data){
-                if(err) console.log(err);
-                makeDir();
-            })
-        }
-    });
+ftpDeploy.deploy(config, function(err) {
+    if (err) console.log(err)
+    else console.log('finished');
 });
 
 
-function makeDir(){
-    Ftp.raw.mkd("/" + process.argv[5], function(err, data) {
-        if (err) return console.error(err);
 
-        console.log(data.text); // Show the FTP response text to the user
-        console.log(data.code); // Show the FTP response code to the user
-
-        traverseFileSystem('dist');
-
-    });
-}
-
-function uploadFile(file){
-    Ftp.put(file, process.argv[5] + "/" + file, function(err) {
-        if (err) return console.error(err);
-        if (!err)
-            console.log("File transferred successfully!");
-    });
-}
-
-function quit(){
-    Ftp.raw.quit(function(err, data) {
-        if (err) return console.error(err);
-
-        console.log("Bye!");
-    });
-}
 
 
