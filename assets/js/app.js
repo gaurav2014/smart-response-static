4ac05ca58adf1dfce2a5cc14ac9c75b311911439
Trey Eckels <trey.eckels@gmail.com>
2014-05-28 00:26:56 -0400

    brought in d3 for charting

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,16 +12,29 @@ require(['framework-config'], function(frameworkConfig){
      */
     require([
         'jquery',
+        'd3',
         'jquery.bootstrap',
         'flexslider'
     ],
-        function($) {
+        function($,d3) {
             $('.flexslider').flexslider({
                 animation: "slide",
                 start: function(){
                     $('#pageCover').hide();
                 }
             });
+            var data = [10];
+
+            var x = d3.scale.linear()
+                .domain([0, d3.max(data)])
+                .range([0, 100]);
+
+            d3.select(".chart")
+                .selectAll("div")
+                .data(data)
+                .enter().append("div")
+                .style("width", function(d) { return x(d) + "px"; })
+                .text(function(d) { return d; });
         });
 });
 
