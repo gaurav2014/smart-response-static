dfb9046e012e895654042764697bc584e842b95e
Trey Eckels <trey.eckels@gmail.com>
2014-05-29 00:16:46 -0400

    brought in d3 for charting

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,18 +23,18 @@ require(['framework-config'], function(frameworkConfig){
                     $('#pageCover').hide();
                 }
             });
-            var data = [10];
 
-            var x = d3.scale.linear()
-                .domain([0, d3.max(data)])
-                .range([0, 100]);
+            $('.chart').each(function(idx){
 
-            d3.select(".chart")
-                .selectAll("div")
-                .data(data)
-                .enter().append("div")
-                .style("width", function(d) { return x(d) + "px"; })
-                .text(function(d) { return d; });
+                var data = [((idx + 3) * 10)];
+                d3.select(this)
+                    .selectAll("div")
+                    .data(data)
+                    .enter().append("div")
+                    .style("width", function(d) { return d + "%"; })
+                    .text(function(d) { return d + '%'; });
+            });
+
         });
 });
 
